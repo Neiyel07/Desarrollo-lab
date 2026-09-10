063d45df7f4bc7dc629e87907af03776f7078187
Neiyel Rodriguez <rneiyel@gmail.com>
2026-09-10 11:09:58 -0600

    d

--- lab2_grupo8/index.php
+++ lab2_grupo8/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Laboratorio 2 - Subida de Archivos</title>
-    <!--  dBootstrap 5 -->
+    <!--  ddBootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="bg-light">
